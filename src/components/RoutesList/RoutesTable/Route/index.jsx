99d11a6f40d503720084602f43a75de3da717c38
V.Sviderski <svidvlad15@gmail.com>
2023-01-09 00:19:26 +0300

    added stopPropagation

--- src/components/RoutesList/RoutesTable/Route/index.jsx
+++ src/components/RoutesList/RoutesTable/Route/index.jsx
@@ -5,15 +5,13 @@ import "./route.less";
 
 const Route = ({ route }) => {
 	const { removeCoordinate, getPolylineAction } = useActions();
+	const onRemoveClick = (e) => {
+		e.stopPropagation();
+		removeCoordinate(route.id);
+	}
 
 	return (
-		<div
-			className="wrap-content"
-			onClick={() => {
-				getPolylineAction(route)
-			}}
-		
-		>
+		<div className="wrap-content" onClick={() => { getPolylineAction(route) }}>
 			<div className="todoItem_lists"  >
 				<div className="todoItem_li">{route.number}</div>
 				<div className="todoItem_li">{route.fromLatitude}</div>
@@ -22,7 +20,7 @@ const Route = ({ route }) => {
 				<div className="todoItem_li">{route.toLongitude}</div>
 				<div className="todoItem_li">
 					<div className="buttons">
-						<button onClick={() => removeCoordinate(route.id)}>
+						<button onClick={onRemoveClick}>
 							<span className="text">
 								<FontAwesomeIcon icon={faTrash} />
 							</span>
