d3d7b4ff257574a4a62bc8eeb88588381f544842
V.Sviderski <svidvlad15@gmail.com>
2023-01-08 16:37:47 +0300

    fixed coordinate

--- src/redux/reducers/coordinatesReducer.js
+++ src/redux/reducers/coordinatesReducer.js
@@ -53,7 +53,7 @@ const initialCoordinates = [
     id: Math.random(),
     number: '7',
     fromLatitude: '50.83567701',
-    fromLongitude: '30.38064206',
+    fromLongitude: '31.38064206',
     toLatitude: '60.84660399',
     toLongitude: '35.29496392',
   },
