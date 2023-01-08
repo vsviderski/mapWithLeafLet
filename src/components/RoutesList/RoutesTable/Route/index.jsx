95afac00d9ee7ae19d134207df12c3fb100f7ae8
V.Sviderski <svidvlad15@gmail.com>
2023-01-09 00:44:03 +0300

    fixed Route.tsx

--- src/components/RoutesList/RoutesTable/Route/index.jsx
+++ src/components/RoutesList/RoutesTable/Route/index.jsx
@@ -1,29 +1,37 @@
 import { useActions } from "hooks/useActions";
- import {DeleteOutlined} from '@ant-design/icons'
+import { DeleteOutlined } from '@ant-design/icons'
 import "./style.less";
 
-const Route = ({ route }) => {
+const Route = ({ route, selectedRoute, setSelectedRoute }) => {
+	const { number, fromLatitude, fromLongitude, toLatitude, toLongitude } = route;
 	const { removeCoordinate, getPolylineAction } = useActions();
+
 	const onRemoveClick = (e) => {
 		e.stopPropagation();
 		removeCoordinate(route.id);
 	}
 
 	return (
-		<div className="wrap-content" onClick={() => { getPolylineAction(route) }}>
-			<div className="todoItem_lists"  >
-				<div className="todoItem_li">{route.number}</div>
-				<div className="todoItem_li">{route.fromLatitude}</div>
-				<div className="todoItem_li">{route.fromLongitude}</div>
-				<div className="todoItem_li">{route.toLatitude}</div>
-				<div className="todoItem_li">{route.toLongitude}</div>
-				<div className="todoItem_li">
-					<div className="buttons">
-						<button onClick={onRemoveClick}>
-							<span className="text">
-							<DeleteOutlined />
-							</span>
-						</button>
+		<div
+			key={route.id}
+			className={selectedRoute === route ? "todoItem_ul-active" : "todoItem_ul"}
+			onClick={() => setSelectedRoute(route)}
+		>
+			<div className="wrap-content" onClick={() => { getPolylineAction(route) }}>
+				<div className="todoItem_lists"  >
+					<div className="todoItem_li">{number}</div>
+					<div className="todoItem_li">{fromLatitude}</div>
+					<div className="todoItem_li">{fromLongitude}</div>
+					<div className="todoItem_li">{toLatitude}</div>
+					<div className="todoItem_li">{toLongitude}</div>
+					<div className="todoItem_li">
+						<div className="buttons">
+							<button onClick={onRemoveClick}>
+								<span className="text">
+									<DeleteOutlined />
+								</span>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/RoutesList/RoutesTable/index.jsx
+++ src/components/RoutesList/RoutesTable/index.jsx
@@ -4,30 +4,29 @@ import Route from "./Route";
 import "./style.less";
 
 const RoutesTable = () => {
-	const routes = useSelector(state => state.coordinates);
-	const [selectedRoute, setSelectedRoute] = useState(null)
+    const routes = useSelector(state => state.coordinates);
+    const [selectedRoute, setSelectedRoute] = useState(null)
 
-	return (
-		<div>
-			{!!routes.length && <div className="todolist">
-				<div className="todolist_item">№</div>
-				<div className="todolist_item">ОТ lat.</div>
-				<div className="todolist_item">ОТ lng.</div>
-				<div className="todolist_item">ДО lat.</div>
-				<div className="todolist_item">ДО lng.</div>
-			</div>
-			}
-			{routes.map((route) => (
-				<div
-					key={route.id}
-					className={selectedRoute === route ? "todoItem_ul-active" : "todoItem_ul"}
-					onClick={() => setSelectedRoute(route)}
-				>
-					<Route route={route} />
-				</div>
-			))}
-		</div>
-	);
+    return (
+        <div>
+            {!!routes.length && <div className="todolist">
+                <div className="todolist_item">№</div>
+                <div className="todolist_item">ОТ lat.</div>
+                <div className="todolist_item">ОТ lng.</div>
+                <div className="todolist_item">ДО lat.</div>
+                <div className="todolist_item">ДО lng.</div>
+            </div>
+            }
+            {routes.map((route) => (
+                <Route
+                    key={route.id}
+                    route={route}
+                    selectedRoute={selectedRoute}
+                    setSelectedRoute={setSelectedRoute}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default RoutesTable;
